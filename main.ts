ce5f93de988a3202e775a9dec203598d288135ca
Evan-Maddicks <evan.maddicks@mths.ca>
2024-09-17 11:16:09 -0400

    Program now does basic math

--- main.ts
+++ main.ts
@@ -1,8 +1,39 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Evan M
+ * Created on: Sep 2024
+ * This program does basic math
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+//Addition
+basic.showString('312 + 42 =' + (312 + 42 ).toString())
+basic.showIcon(IconNames.Happy)
+basic.clearScreen()
+basic.pause(1000)
+
+//Multiplication
+basic.showString('100 x 30 =' + (100 * 30).toString())
+basic.showIcon(IconNames.Heart)
+basic.clearScreen()
+basic.pause(1000)
+
+//Subtration
+basic.showString('100 - 50 =' + (100 - 50).toString())
+basic.showIcon(IconNames.Fabulous)
+basic.clearScreen()
+basic.pause(1000)
+
+//Division
+basic.showString('50 / 5 =' + (50 / 5).toString())
+basic.showIcon(IconNames.SmallHeart)
+basic.clearScreen()
+basic.pause(1000)
+
+//Exponents
+basic.showString('10 + 5^5 =' + (50 + 5**5).toString())
+basic.showIcon(IconNames.Surprised)
+basic.clearScreen()
+basic.pause(1000)
